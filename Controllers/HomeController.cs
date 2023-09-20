51a0583b2b3e90979a8addb5b8852986f556397e
Dharmendra Singh <88479174+dsdevops50@users.noreply.github.com>
2023-09-20 17:49:54 +0530

    Add files via upload
    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using DSWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var details = new Details();
+            details.Env = _configuration.GetValue<String>("Environment");
+            return View(details);
+
+        }
+        
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Details.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSWebApp.Models
+{
+    public class Details
+    {
+        public string Env 
+        { 
+            get; 
+            set; 
+        }
+    }
+}
